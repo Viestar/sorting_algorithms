86c83e21b439832076d861a57288eb06cb88eb1f
Viestar <m.silverbuckson@gmail.com>
2023-06-25 00:56:36 +0300

    0-bubble_sort.c

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-![sort algorithm](assets/sort.png)
+![sort algorithm](assets/sort.png)## 0. Bubble sort - compare two adjacents integers and swaps them if not in order
--- sort.h
+++ sort.h
@@ -1,9 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
-/* Prototypes */
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+#include <stdio.h>
+#include <stdbool.h>
 
 /* Data Structures */
 
@@ -20,5 +18,10 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+/* Prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H*/
