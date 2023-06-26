c9ddd783e8632bd4b2a29c61d0c576c584a305b6
Viestar <m.silverbuckson@gmail.com>
2023-06-26 04:38:39 +0300

    0-bubble_sort.c ---betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,26 +7,26 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-bool no_more_swaps;
-size_t index, i;
-int temp;
+	bool no_more_swaps;
+	size_t index, i;
+	int temp;
 
-(void)no_more_swaps;
-/* Check if the array is not empty */
-if (array == NULL)
-return;
+	(void)no_more_swaps;
+	/* Check if the array is not empty */
+	if (array == NULL)
+		return;
 
-for (i = 0; i < (size - 1); i++)
-{
-for (index = 0; index < (size - 1); index++)
-{
-if (array[index] > array[index + 1])
-{
-temp = array[index];
-array[index] = array[index + 1];
-array[index + 1] = temp;
-print_array(array, size);
-}
-}
-}
+	for (i = 0; i < (size - 1); i++)
+	{
+		for (index = 0; index < (size - 1); index++)
+		{
+			if (array[index] > array[index + 1])
+			{
+				temp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
