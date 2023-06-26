6922b8d4632648a597c27876c7932cdb9761eff0
Viestar <m.silverbuckson@gmail.com>
2023-06-26 04:32:07 +0300

    1-insertion_sort_list.c --- Betty styled

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,35 +7,35 @@
 
 void insertion_sort_list(listint_t **list)
 {
-listint_t *temp_node = NULL;
-listint_t *local = *list;
+	listint_t *temp_node = NULL;
+	listint_t *local = *list;
 
-/* Checking if array is empty */
-if (local == NULL || local->next == NULL)
-return;
+	/* Checking if array is empty */
+	if (local == NULL || local->next == NULL)
+		return;
 
-local = local->next;
+	local = local->next;
 
-/* Insertion sort procedures */
-while (local)
-{
-while (local->prev && local->n < local->prev->n)
-{
-temp_node = local;
-if (local->next)
-local->next->prev = temp_node->prev;
-local->prev->next = temp_node->next;
-local = local->prev;
-temp_node->prev = local->prev;
-temp_node->next = local;
-if (local->prev)
-local->prev->next = temp_node;
-local->prev = temp_node;
-if (temp_node->prev == NULL)
-*list = temp_node;
-print_list(*list);
-local = local->prev;
-}
-local = local->next;
-}
+	/* Insertion sort procedures */
+	while (local)
+	{
+		while (local->prev && local->n < local->prev->n)
+		{
+			temp_node = local;
+			if (local->next)
+				local->next->prev = temp_node->prev;
+			local->prev->next = temp_node->next;
+			local = local->prev;
+			temp_node->prev = local->prev;
+			temp_node->next = local;
+			if (local->prev)
+				local->prev->next = temp_node;
+			local->prev = temp_node;
+			if (temp_node->prev == NULL)
+				*list = temp_node;
+			print_list(*list);
+			local = local->prev;
+		}
+		local = local->next;
+	}
 }
