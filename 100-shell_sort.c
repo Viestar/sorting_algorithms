cb4ad0d4fe6417c53c8ddc9cbd5730ddedffc8c8
Viestar <m.silverbuckson@gmail.com>
2023-06-26 18:01:11 +0300

    100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,33 +8,33 @@
 
 void shell_sort(int *array, size_t size)
 {
-    unsigned int gap, index, j;
-    int temp_cont = 0;
+	int temp_cont = 0;
+	unsigned int gap, index, j;
 
-    gap = index = j = 0;
+	gap = index = j = 0;
 
-    /* Incase of an empty array or 1 item array */
-    if (array == NULL || size < 2)
-        return;
+	/* Incase of an empty array or 1 item array */
+	if (array == NULL || size < 3)
+		return;
 
-    while (gap < size / 2)
-        gap = gap * 2 + 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (index = gap; index < size; index++)
-        {
-            temp_cont = array[index];
-            for (j = index; j >= gap && array[j - gap] > temp_cont;
-                 j -= gap)
-            {
-                if (array[j] != array[j - gap])
-                    array[j] = array[j - gap];
-            }
-            if (array[j] != temp_cont)
-                array[j] = temp_cont;
-        }
-        print_array(array, size);
-        gap = (gap - 1) / 2;
-    }
+	while (gap > 0)
+	{
+		for (index = gap; index < size; index++)
+		{
+			temp_cont = array[index];
+			for (j = index; j >= gap && array[j - gap] > temp_cont;
+				 j -= gap)
+			{
+				if (array[j] != array[j - gap])
+					array[j] = array[j - gap];
+			}
+			if (array[j] != temp_cont)
+				array[j] = temp_cont;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
 }
